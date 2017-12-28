f6ab1ce6a77971d569dba3a4344852c92a0ddbc1
ahmed miladi <ahess110@gmail.com>
2017-12-28 03:05:37 +0100

    make it better

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask (__name__)
 PORT = 5000
@@ -8,9 +8,14 @@ DEBUG = False
 def not_found(error):
     return "Not Found."
 
-@app.route('/', methods=['GET'])
+@app.route("/")
 def index():
-    return "my name is ahmed miladi"
+    return "Hello World!"
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return "i got you question"
 
 if __name__ == '__main__' :
     app.run(port = PORT , debug = DEBUG)
